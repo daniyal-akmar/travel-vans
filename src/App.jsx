43ca7ada17cc01f1a5013afeff774a052784655b
Daniyal Khan <emdaniyal24@gmail.com>
2023-06-11 13:19:20 +0500

    css improved

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Vans from "./pages/Vans";
 
 const App = () => {
     return (
-        <>
+        <div className="app">
             <header className="site-header">
                 <div className="container container-flex">
                     <div className="logo">
@@ -13,7 +14,7 @@ const App = () => {
                     
                     <nav className="site-nav">
                         <Link to="/about">About</Link>
-                        <Link to="/about">Vans</Link>
+                        <Link to="/vans">Vans</Link>
                     </nav>
                 </div>
             </header>
@@ -21,12 +22,13 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/vans" element={<Vans />} />
             </Routes>
 
             <footer className="site-footer">
                 <p className="small-copy">&copy; 2022 #VANLIFE</p>
             </footer>
-        </>
+        </div>
     )
 }
 
---
+++ src/pages/Vans.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Vans = () => {
+  return (
+    <div className="vans">
+        <div className="container">
+            <h1>Vans page goes here</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Vans;
